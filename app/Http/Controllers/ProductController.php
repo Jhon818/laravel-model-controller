f52c138c40da4056ec1f9ddd6f362c5bcc57a56a
giov <pintogiovanni818@gmail.com>
2021-10-28 17:30:18 +0200

    ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function listProducts() {
+        Product::all();
+        $paste = Product::all();
+        return view('products', compact('paste'));
+    }
+
+    public function productDetail($id) {
+        /* Select * FROM products WHERE id = $id */
+        //$product = Product::where('id', $id)->first();
+        $product = Product::find($id);
+
+        return view('dettagli', compact('product'));
+    }
 }
